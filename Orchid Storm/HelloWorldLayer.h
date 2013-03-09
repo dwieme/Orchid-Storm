1b4f1162932be44e38f7dabed417c5d0dc425e3c
Dan Wieme <dwieme@ragebook.rit.edu>
2013-03-09 14:55:51 -0500

    Initial Commit

---
+++ Orchid Storm/HelloWorldLayer.h
@@ -0,0 +1,23 @@
+//
+//  HelloWorldLayer.h
+//  Orchid Storm
+//
+//  Created by Dan Wieme on 3/9/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+
+#import <GameKit/GameKit.h>
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
